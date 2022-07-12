9a36829e2a39443bf66a780ca448b373d17601ac
Dhiego Silva <dhiego.silva@esab.de>
2022-07-12 13:41:07 +0200

    update

--- src/main/java/com/application/views/main/Login.java
+++ src/main/java/com/application/views/main/Login.java
@@ -2,46 +2,33 @@ package com.application.views.main;
 
 import com.application.video.Video;
 import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.IFrame;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.dom.ThemeList;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.Lumo;
 
 @PageTitle("Degussa")
 @Route(value = "")
-//@JsModule("./js/prefers-color-scheme.js")
 
 public class Login extends VerticalLayout {
 	
-    private Button toggleThemeButton = new Button("Dark theme");
 	private Button enterBtn = new Button("Enter");
     private Image img = new Image("https://upload.wikimedia.org/wikipedia/commons/5/56/Degussa_logo.png", "placeholder plant");
-
+    private Video video = new Video();
+    
     public Login() {
 		
-		Video video = new Video("/mytodo/src/main/resources/videoplayback.webm");
-        video.setMaxWidth("500px");
-        add(video);
-		
-		
-		
-//		initView();
-//		String iframe = "<iframe style=\"position: absolute; top: 0; left: 0; width: 100%; height: 100%;\" width=\"1280\" height=\"720\" src=\"https://www.youtube.com/embed/7Jxdj0tp-Z0?autoplay=1&mute=1&disablekb=1&controls=0&loop=1\" frameborder=\"0\"></iframe>\r\n";
-//		
-//		Label content = new Label();
-//		content.getElement().setProperty("innerHTML", iframe);
-//		add(content);
-		
+    	video.setId("degussaVideo");
+    	video.setAutoPlay(true);
+    	video.setLoop(true);
+        video.setControls(false);
+    	video.setSource("https://github.com/dhiegosilva/learninVaadin/blob/main/src/main/resources/videoplayback.webm?raw=true");
+	
+    	add(video);
+    	
         img.setId("degussaLogo");
-
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
@@ -54,23 +41,9 @@ public class Login extends VerticalLayout {
         enterBtn.addClickShortcut(Key.ENTER);
 
         enterBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-	    toggleThemeButton.addClickListener(click->{
-	    ThemeList themeList = UI.getCurrent().getElement().getThemeList(); // (1)
-	
-	          if (themeList.contains(Lumo.DARK)) { // (2)
-	            themeList.remove(Lumo.DARK);
-	            toggleThemeButton.setText("Dark Theme");    
-	          } else {
-	            themeList.add(Lumo.DARK);
-	            toggleThemeButton.setText("Light Theme");    
-	          }
-	    });
 	    
-        add(img, enterBtn, toggleThemeButton);
+        add(img, enterBtn);
+        
     }
-	private void initView() {
-		setDefaultHorizontalComponentAlignment(Alignment.STRETCH);
-	}
 
 }
